eb7ba95e0f4ee445167a3bfd18512d9708b7b692
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-17 17:52:31 +0600

    custombtn woriking

--- lib/widgets/custom_btn.dart
+++ lib/widgets/custom_btn.dart
@@ -6,17 +6,22 @@ class CustomBtn extends StatelessWidget {
     return Container(
       height: 60,
       alignment: Alignment.center,
-      
       decoration: BoxDecoration(
         border: Border.all(
           color: Colors.black,
           width: 2.0,
         ),
         borderRadius: BorderRadius.circular(12.0),
-
       ),
-      margin: EdgeInsets.symmetric(horizontal: 24.0,vertical: 24.0),
-      child: Text("Create Account button"),
+      margin: EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+      child: Text(
+        "Create New Account",
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 16.0,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
