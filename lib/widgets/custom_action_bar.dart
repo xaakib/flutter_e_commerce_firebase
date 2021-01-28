425daf8dd17cd73c6c22842c094cc259367ee9b7
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-28 18:42:05 +0600

    added backarrow image and woriking other widgets

--- lib/widgets/custom_action_bar.dart
+++ lib/widgets/custom_action_bar.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import '../constant.dart';
 
 class CustomActionBar extends StatelessWidget {
+  final String title;
+  final bool hasBackArrow;
+  final bool hasTile;
+
+  const CustomActionBar({this.title, this.hasBackArrow, this.hasTile});
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,7 +19,22 @@ class CustomActionBar extends StatelessWidget {
             "Action Bar",
             style: Constants.boldheading,
           ),
-          Text("0"),
+          Container(
+              height: 42,
+              width: 42,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.black),
+              child: Center(
+                child: Text(
+                  "0",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )),
         ],
       ),
     );
