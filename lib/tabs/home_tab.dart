98932c59fab43567f7cbcf46a74156a09587962b
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-30 14:03:40 +0600

    some work are done

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce_firebase/constant.dart';
 import 'package:flutter_e_commerce_firebase/widgets/custom_action_bar.dart';
 
 class HomeTab extends StatelessWidget {
@@ -30,23 +31,48 @@ class HomeTab extends StatelessWidget {
                     children: snapshot.data.docs.map((document) {
                       return Container(
                         margin: EdgeInsets.symmetric(
-                          horizontal: 12.0,vertical: 12.0
-                        ),
+                            horizontal: 12.0, vertical: 12.0),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0)),
                         height: 300,
-                        child: Column(
+                        child: Stack(
                           children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: Image.network(
-                                document.data()['images'][0],
-                                fit: BoxFit.cover,
+                            Container(
+                              height: 300,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: Image.network(
+                                  document.data()['images'][0],
+                                  fit: BoxFit.cover,
 
-                                // height: 200,
-                                // width: MediaQuery.of(context).size.width,
+                                ),
                               ),
                             ),
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(24.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      document.data()['name'],
+                                      style: Constants.regulerheading,
+                                    ),
+                                    Text(
+                                        "\$${document.data()['price']}" ??
+                                            "Price",
+                                        style: TextStyle(
+                                            fontSize: 18.0,
+                                            color: Colors.redAccent,
+                                            fontWeight: FontWeight.w600)),
+                                  ],
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       );
