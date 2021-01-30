f536830bb167dfdd1a9571fd80dbe538fefc81b8
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-30 19:11:40 +0600

    some document is done

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce_firebase/widgets/custom_action_bar.dart';
 
-import '../constant.dart';
 
 class ProductPage extends StatefulWidget {
   final String productId;
@@ -18,48 +17,52 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          FutureBuilder(
-            future: _productsRef.doc(widget.productId).get(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
+      backgroundColor: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Stack(
+          children: [
+            FutureBuilder(
+              future: _productsRef.doc(widget.productId).get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Scaffold(
+                    body: Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    ),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                // Collection Data ready to display
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Map<String, dynamic> documentData = snapshot.data.data();
+                  return ListView(
+                    children: [
+                      Image.network(
+                        "${documentData['images'][0]}",
+                      ),
+                    ],
+                  );
+                }
+
+                // Loading State
                 return Scaffold(
                   body: Center(
-                    child: Text("Error: ${snapshot.error}"),
+                    child: CircularProgressIndicator(),
                   ),
                 );
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              // Collection Data ready to display
-              if (snapshot.connectionState == ConnectionState.done) {
-                Map<String, dynamic> documentData = snapshot.data.data();
-                return ListView(
-                  children: [
-                    Image.network(
-                      "${documentData['images'][0]}",
-                    ),
-                  ],
-                );
-              }
-
-              // Loading State
-              return Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            },
-          ),
-          CustomActionBar(
-            hasTitle: false,
-            title: "Home",
-            hasBackArrrow: true,
-          ),
-        ],
+              },
+            ),
+            CustomActionBar(
+              hasTitle: false,
+              title: "Home",
+              hasBackArrrow: true,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -10,104 +10,107 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          FutureBuilder<QuerySnapshot>(
-            future: _productsRef.get(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
+    return SafeArea(
+          child: Container(
+        child: Stack(
+          children: [
+            FutureBuilder<QuerySnapshot>(
+              future: _productsRef.get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Scaffold(
+                    body: Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    ),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                // Collection Data ready to display
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // Display the data inside a list view
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: ListView(
+                      children: snapshot.data.docs.map((document) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ProductPage(
+                                          productId: document.id,
+                                          
+                                        )));
+                          },
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 12.0, vertical: 12.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0)),
+                            height: 300,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  height: 300,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    child: Image.network(
+                                      document.data()['images'][0],
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  left: 0,
+                                  right: 0,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(24.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          document.data()['name'],
+                                          style: Constants.regulerheading,
+                                        ),
+                                        Text(
+                                            "\$${document.data()['price']}" ??
+                                                "Price",
+                                            style: TextStyle(
+                                                fontSize: 18.0,
+                                                color: Colors.redAccent,
+                                                fontWeight: FontWeight.w600)),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                }
+
+                // Loading State
                 return Scaffold(
                   body: Center(
-                    child: Text("Error: ${snapshot.error}"),
+                    child: CircularProgressIndicator(),
                   ),
                 );
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              // Collection Data ready to display
-              if (snapshot.connectionState == ConnectionState.done) {
-                // Display the data inside a list view
-                return Center(
-                  child: ListView(
-                    children: snapshot.data.docs.map((document) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ProductPage(
-                                        productId: document.id,
-                                        
-                                      )));
-                        },
-                        child: Container(
-                          margin: EdgeInsets.symmetric(
-                              horizontal: 12.0, vertical: 12.0),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.0)),
-                          height: 300,
-                          child: Stack(
-                            children: [
-                              Container(
-                                height: 300,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(12.0),
-                                  child: Image.network(
-                                    document.data()['images'][0],
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        document.data()['name'],
-                                        style: Constants.regulerheading,
-                                      ),
-                                      Text(
-                                          "\$${document.data()['price']}" ??
-                                              "Price",
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: Colors.redAccent,
-                                              fontWeight: FontWeight.w600)),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                );
-              }
-
-              // Loading State
-              return Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            },
-          ),
-          CustomActionBar(
-            title: "Home",
-            hasBackArrrow: false,
-          ),
-        ],
+              },
+            ),
+            CustomActionBar(
+              title: "Home",
+              hasBackArrrow: false,
+            ),
+          ],
+        ),
       ),
     );
   }
