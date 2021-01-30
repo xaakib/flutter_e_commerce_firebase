9cc4cebd6d0611a5aeef8b0d0a556c93c45cdd9d
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-30 15:28:04 +0600

    details page are done to data fetching

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,13 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce_firebase/widgets/custom_action_bar.dart';
+
+import '../constant.dart';
 
 class ProductPage extends StatefulWidget {
+  final String productId;
+
+  const ProductPage({this.productId});
   @override
   _ProductPageState createState() => _ProductPageState();
 }
 
 class _ProductPageState extends State<ProductPage> {
+  final CollectionReference _productsRef =
+      FirebaseFirestore.instance.collection("Products");
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: _productsRef.doc(widget.productId).get(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Scaffold(
+                  body: Center(
+                    child: Text("Error: ${snapshot.error}"),
+                  ),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              // Collection Data ready to display
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> documentData = snapshot.data.data();
+                return ListView(
+                  children: [
+                    Image.network(
+                      "${documentData['images'][0]}",
+                    ),
+                  ],
+                );
+              }
+
+              // Loading State
+              return Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+          ),
+          CustomActionBar(
+            hasTitle: false,
+            title: "Home",
+            hasBackArrrow: true,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce_firebase/constant.dart';
+import 'package:flutter_e_commerce_firebase/screens/product_page.dart';
 import 'package:flutter_e_commerce_firebase/widgets/custom_action_bar.dart';
 
 class HomeTab extends StatelessWidget {
@@ -32,50 +33,61 @@ class HomeTab extends StatelessWidget {
                 return Center(
                   child: ListView(
                     children: snapshot.data.docs.map((document) {
-                      return Container(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 12.0),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12.0)),
-                        height: 300,
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 300,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  document.data()['images'][0],
-                                  fit: BoxFit.cover,
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ProductPage(
+                                        productId: document.id,
+                                        
+                                      )));
+                        },
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 12.0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12.0)),
+                          height: 300,
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 300,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                  child: Image.network(
+                                    document.data()['images'][0],
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Padding(
-                                padding: const EdgeInsets.all(24.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      document.data()['name'],
-                                      style: Constants.regulerheading,
-                                    ),
-                                    Text(
-                                        "\$${document.data()['price']}" ??
-                                            "Price",
-                                        style: TextStyle(
-                                            fontSize: 18.0,
-                                            color: Colors.redAccent,
-                                            fontWeight: FontWeight.w600)),
-                                  ],
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(24.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        document.data()['name'],
+                                        style: Constants.regulerheading,
+                                      ),
+                                      Text(
+                                          "\$${document.data()['price']}" ??
+                                              "Price",
+                                          style: TextStyle(
+                                              fontSize: 18.0,
+                                              color: Colors.redAccent,
+                                              fontWeight: FontWeight.w600)),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       );
                     }).toList(),
