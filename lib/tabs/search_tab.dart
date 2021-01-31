ec2e64f1b431b785f0baed799da82f7edc0cee00
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-01-31 22:38:33 +0600

    successuflly done flutter firebase e commerce app

--- lib/tabs/search_tab.dart
+++ lib/tabs/search_tab.dart
@@ -1,16 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce_firebase/services/firebase_serviecs.dart';
+import 'package:flutter_e_commerce_firebase/widgets/custom_input.dart';
+import 'package:flutter_e_commerce_firebase/widgets/product_card.dart';
 
 import '../constant.dart';
 
-class SearchTab extends StatelessWidget {
+class SearchTab extends StatefulWidget {
+  @override
+  _SearchTabState createState() => _SearchTabState();
+}
+
+class _SearchTabState extends State<SearchTab> {
+  FirebaseServies _firebaseServices = FirebaseServies();
+  String _searchString = "";
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-          child: Text(
-        "Search Tab",
-        style: Constants.regulerheading,
-      )),
+      child: Stack(
+        children: [
+          if (_searchString.isEmpty)
+            Center(
+              child: Container(
+                child: Text(
+                  "Search Results",
+                  style: Constants.regulerDarkText,
+                ),
+              ),
+            )
+          else
+            FutureBuilder<QuerySnapshot>(
+              future: _firebaseServices.productsRef
+                  .orderBy("searching")
+                  .startAt([_searchString]).endAt(
+                      ["$_searchString\uf8ff"]).get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Scaffold(
+                    body: Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    ),
+                  );
+                }
+
+                // Collection Data ready to display
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // Display the data inside a list view
+                  return ListView(
+                    padding: EdgeInsets.only(
+                      top: 128.0,
+                      bottom: 12.0,
+                    ),
+                    children: snapshot.data.docs.map((document) {
+                      return ProductCard(
+                        title: document.data()['name'],
+                        imageUrl: document.data()['images'][0],
+                        price: "\$${document.data()['price']}",
+                        productId: document.id,
+                      );
+                    }).toList(),
+                  );
+                }
+
+                // Loading State
+                return Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 45.0,
+            ),
+            child: CustomInput(
+              hintText: "Search here...",
+              onSubmit: (value) {
+                setState(() {
+                  _searchString = value.toLowerCase();
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,75 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce_firebase/screens/product_page.dart';
+
+import '../constant.dart';
+
+class ProductCard extends StatelessWidget {
+  final String productId;
+  final Function onPressed;
+  final String imageUrl;
+  final String title;
+  final String price;
+  ProductCard({this.onPressed, this.imageUrl, this.title, this.price, this.productId});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => ProductPage(productId: productId,),
+        ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        height: 350.0,
+        margin: EdgeInsets.symmetric(
+          vertical: 12.0,
+          horizontal: 24.0,
+        ),
+        child: Stack(
+          children: [
+            Container(
+              height: 350.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image.network(
+                  "$imageUrl",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Row(
+                  mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      title,
+                      style: Constants.regulerheading,
+                    ),
+                    Text(
+                      price,
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).accentColor,
+                          fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
